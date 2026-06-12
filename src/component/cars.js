e70297983c4d078d2b0ed52696a6f968b988320d
KAVYA SHRI A <717824f229@kce.ac.in>
2026-06-12 13:59:24 +0530

    Create cars.js

---
+++ src/component/cars.js
@@ -0,0 +1,24 @@
+const cars=[{
+    name:"Mercedes Benz",
+    color:"Black",
+    url:"https://images.pexels.com/photos/31510208/pexels-photo-31510208.jpeg",
+    price:"Rs.500000"
+},
+{
+    name:"Audi",
+    color:"White",
+    url:"https://images.pexels.com/photos/1005634/pexels-photo-1005634.jpeg",
+    price:"Rs.350000"
+},
+{name:" Maruti Suzuki",
+    color:"Red",
+    url:"https://images.pexels.com/photos/10944397/pexels-photo-10944397.jpeg",
+    price:"Rs.250000"
+},
+{
+    name:"Honda City",
+    color:"Blue",
+    url:"https://images.pexels.com/photos/14073070/pexels-photo-14073070.jpeg",
+    price:"Rs.200000"
+}]
+export default cars
