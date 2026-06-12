99015d0eda5d46e63287f2c29647bb30bc6f8272
KAVYA SHRI A <717824f229@kce.ac.in>
2026-06-12 13:59:54 +0530

    Create trucks.js

---
+++ src/component/trucks.js
@@ -0,0 +1,25 @@
+const trucks=[{
+    name:"Tata Motors",
+    color:"Yellow",
+    url:"https://images.pexels.com/photos/37627322/pexels-photo-37627322.jpeg",
+    price:"Rs.3900000"
+},
+{
+    name:"Ashok Leyland",
+    color:"White",
+    url:"https://images.pexels.com/photos/20862827/pexels-photo-20862827.jpeg",
+    price:"Rs.2000000"
+},
+{
+    name:"BharatBenz",
+    color:"Blue",
+    url:"https://images.pexels.com/photos/28520996/pexels-photo-28520996.jpeg",
+    price:"Rs.1500000"
+},
+{
+    name:"Mahindra",
+    color:"Red",
+    url:"https://images.pexels.com/photos/9754798/pexels-photo-9754798.jpeg",
+    price:"Rs.1000000"
+}]
+export default trucks
