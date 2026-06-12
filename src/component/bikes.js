c593e19d3cf3a0b3f34926584526779f840d1113
KAVYA SHRI A <717824f229@kce.ac.in>
2026-06-12 13:58:52 +0530

    Create bikes.js

---
+++ src/component/bikes.js
@@ -0,0 +1,25 @@
+const bikes=[{
+    name:"Royal Enfield",
+    color:"Black",
+    url:"https://images.pexels.com/photos/18749193/pexels-photo-18749193.jpeg",
+    price:"Rs.186000"
+},
+{
+    name:"Yamaha R15 V4",
+    color:"Blue",
+    url:"https://images.pexels.com/photos/19435687/pexels-photo-19435687.jpeg",
+    price:"Rs.170000"
+},
+{
+    name:"TVS iQube",
+    color:"Red",
+    url:"https://images.pexels.com/photos/11094871/pexels-photo-11094871.jpeg",
+    price:"Rs.113000"
+},
+{
+    name:"Hero Splendor Plus",
+    color:"White",
+    url:"https://images.pexels.com/photos/11094204/pexels-photo-11094204.png",
+    price:"Rs.77000"
+}]
+export default bikes
