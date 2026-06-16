d7d1f42599a56ce1f0ffdac2b89c4d01206c2626
KAVYA SHRI A <717824f229@kce.ac.in>
2026-06-16 15:32:29 +0530

    Create students.js

---
+++ src/component/students.js
@@ -0,0 +1,27 @@
+const students=[{
+    regno:"717824F229",
+    name:"Kavya",
+    dept:"IT",
+    year:"III",
+    subjects:[
+    {name:"React",marks:90},
+    {name:"WAD",marks:80},
+    {name:"DBMS",marks:95},
+    {name:"Java",marks:82},
+    {name:"DS",marks:78}
+]
+},{
+    regno:"717824P101",
+    name:"Shri",
+    dept:"CSE",
+    year:"II",
+    subjects:[
+        {name:"React",marks:75},
+        {name:"WAD",marks:69},
+        {name:"DBMS",marks:89},
+        {name:"Java",marks:92},
+        {name:"DS",marks:78}
+    ]
+}
+]
+export default students
